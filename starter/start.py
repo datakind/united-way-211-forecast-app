e58c818d9146ba43d25ddc5ebad9e37b9b2e8c3f
Jay Justice <masterjx9@gmail.com>
2022-09-23 11:06:57 -0700

    start.py

--- starter/start.py
+++ starter/start.py
@@ -1,3 +1,3 @@
-from subprocess import run
+import subprocess
 import os
-run(["gunicorn", "-k", "eventlet", "-w", "3", "app:app", "-b", "0.0.0.0:"+os.environ.get("PORT")],shell=True,bufsize=1)
+subprocess.Popen(["gunicorn", "-k", "eventlet", "-w", "3", "app:app", "-b", "0.0.0.0:"+os.environ.get("PORT")],stdout=subprocess.PIPE,shell=True,bufsize=1)
