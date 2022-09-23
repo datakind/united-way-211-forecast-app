085b75e20c287d5568e00580fb838b26e9c6e9c9
Jay Justice <masterjx9@gmail.com>
2022-09-23 10:43:23 -0700

    changed shell to true

--- starter/start.py
+++ starter/start.py
@@ -1,3 +1,3 @@
 from subprocess import Popen
 import os
-Popen(["gunicorn", "-k", "eventlet", "-w", "3", "app:app", "-b", "0.0.0.0:"+os.environ.get("PORT")],bufsize=1)
+Popen(["gunicorn", "-k", "eventlet", "-w", "3", "app:app", "-b", "0.0.0.0:"+os.environ.get("PORT")],shell=True,bufsize=1)
