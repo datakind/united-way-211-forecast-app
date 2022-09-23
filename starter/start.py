2bba4fdf943a58f14f28595aa38aa3c6d0cd184a
Jay Justice <masterjx9@gmail.com>
2022-09-23 10:57:24 -0700

    added run module

--- starter/start.py
+++ starter/start.py
@@ -1,3 +1,3 @@
-from subprocess import Popen
+from subprocess import run
 import os
-Popen(["gunicorn", "-k", "eventlet", "-w", "3", "app:app", "-b", "0.0.0.0:"+os.environ.get("PORT")],shell=True,bufsize=1)
+run(["gunicorn", "-k", "eventlet", "-w", "3", "app:app", "-b", "0.0.0.0:"+os.environ.get("PORT")],shell=True,bufsize=1)
